e82677a5aff92f49c5c1fcb2a98f7f8c82b4d89e
pawelsawicz <pawel@sawicz.eu>
2015-11-16 17:06:36 +0000

    add dummy test to fake test coverage

---
+++ MutationTalk.Tests/SimpleIfDummyTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MutationTalk.Tests
+{
+    class SimpleIfDummyTests
+    {
+        [Test]
+        public void CalculateCO2Emmision_EngineSize3_Return200_DummyTest()
+        {
+            //arrange
+            var checker = new SimpleIf();
+
+            //act
+            checker.CalculateCO2Emmision(-999999999);
+
+            //assert
+            Assert.That(true);
+        }
+
+        [Test]
+        public void CalculateCO2Emmision_EngineSize3_Return200_DummyTest2()
+        {
+            //arrange
+            var checker = new SimpleIf();
+
+            //act
+            checker.CalculateCO2Emmision(99999999);
+
+            //assert
+            Assert.That(true);
+        }
+    }
+}
